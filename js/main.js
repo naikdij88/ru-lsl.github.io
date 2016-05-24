eede15e241bd63392081ca75619b7504c5231dcb
naikdij88 <eugeny-nikolaeff@mail.ru>
2016-05-24 16:49:12 +0600

    lsl
    lsl

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+
+
+$(function () {
+	$.getJSON("function.json", function (data) {
+		var template = $('#functpl').html();
+		var html = Mustache.render(template, data);
+		$('#acco').html(html);
+	});
+});
+
+$(function () {
+	$('#accordion')
+	.accordion({
+		header : "> div > h3"
+	})
+	.sortable({
+		axis : "y",
+		handle : "h3",
+		stop : function (event, ui) {
+			// IE doesn't register the blur when sorting
+			// so trigger focusout handlers to remove .ui-state-focus
+			ui.item.children("h3").triggerHandler("focusout");
+
+			// Refresh accordion to handle new order
+			$(this).accordion("refresh");
+		}
+	});
+
+	$("#radio").buttonset();
+});
