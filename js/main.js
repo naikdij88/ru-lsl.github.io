d0d87c9071544803c4e855ea10162bd7e7c82e5a
ru-lsl.github.io <naikdij88@gmail.com>
2016-06-02 23:45:07 +0600

    lsl

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $(function() {
 $(window).load(function() {
     "use strict";
     //$(".cssload-thecube").fadeOut();
-    $("#preloader").delay(350).fadeOut("slow");
+    $("#preloader").delay(2000).fadeOut("slow");
 });
 /* Preloder End
 -------------------------------------------------------------------*/
