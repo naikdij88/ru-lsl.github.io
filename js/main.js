2f5d57b6ac07f411e7dd0eb84dd252efaaa49b57
ru-lsl.github.io <naikdij88@gmail.com>
2016-06-07 21:44:21 +0600

    lsl

--- js/main.js
+++ js/main.js
@@ -26,7 +26,37 @@ $(function() {
 
     });
 
+    $.getJSON("const.json", function (data) {
+        console.log('000: '+data);
+        $('.hr-pk').click(function (e) {
+            e.preventDefault(); // Предотвратить href от перенаправления напрямую
+            //var linkURL = $(this).attr("href");
 
+            var myExp = $(this).attr("href"); //new RegExp(linkURL, "i");
+            var output = '';
+            $.each(data.cans, function (key, val) {
+                console.log(val.name.search(myExp));
+                if (val.name.search(myExp) === 0) {
+                    output += '<div class="name-f ">';
+                    output += '<h2><span class="txtsh">' + val.name + '</span></h2></div>';
+                    output += '<ul class="list-group material-list-group material-list-group">';
+                    output += '<li class="list-group-item material-list-group__item">' + val.opesanie + '</li></ul>';
+                    output += '<button class="btn material-btn material-btn_danger glyphicon glyphicon-eye-open" data-toggle="modal" data-target="#myModal_danger"> WIKI</button>';
+                    output += '<div class="modal material-modal material-modal_danger fade" id="myModal_danger"><div class="modal-dialog ">';
+                    output += '<div class="modal-content material-modal__content"><div class="modal-header material-modal__header">';
+                    output += '<h4 class="modal-title material-modal__title">' + val.name + '</h4></div>';
+                    output += '<div class="modal-body material-modal__body" style="padding: 0;"><div class="embed-responsive embed-responsive-16by9">';
+                    output += '<iframe class="embed-responsive-item" src="https://wiki.secondlife.com/wiki/' + val.name + '"></iframe></div></div>';
+                    output += '<div class="modal-footer material-modal__footer"><button class="btn material-btn " data-dismiss="modal">Close</button>';
+                    output += '</div></div></div></div><div style="display: none;">';
+                }
+            });
+            output += '</div>';
+            $('.update').html(output);
+            //console.log(output);
+
+        });
+    });
 
     $.getJSON("events.json", function(data) {
         var template = $('#event').html();
